9b28bc2725c7ef10b6ddf3eec5b8b167bda31185
omar merghany <omar.merghany95@gmail.com>
2020-11-06 00:08:44 +0200

    The Egyptian Olympiad in Informatics EOI new ladder

---
+++ A2oj_Ladders/The_Egyptian_Olympiad_in_Informatics_EOI/Hello_ProblemSet.cpp
@@ -0,0 +1,27 @@
+//
+// Created by OmarMerghany on 11/6/2020.
+//
+
+/*
+    Problem Statement:
+Welcome to the EOI Ladder. In order to use the Ladder you have to say Hello to the A2 Online Judge that hosts the EOI ladder. The only way to do so is by writing a program that prints "Hello A2OJ".
+
+    Input Format:
+Hello Contestant
+
+    Output Format:
+Hello A2OJ
+
+    Sample Input:
+Hello EOIer
+
+    Sample Output:
+Hello A2OJ
+ */
+#include <iostream>
+using namespace std;
+int main() {
+    string t;
+    cin >> t;
+    cout << "Hello A2OJ" << endl;
+}
